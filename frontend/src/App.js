3a31906484660aa49a614063359839cd7f6cb5fa
novikhasiregar <novika.siregar@truslogic.co.id>
2025-07-20 00:52:44 +0700

    update be

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-require('dotenv').config();
 const backendURL = process.env.REACT_APP_BACKEND_URL || "http:://localhost:5000";
 
 function App() {
